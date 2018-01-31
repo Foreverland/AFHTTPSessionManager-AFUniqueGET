b251d530dc688c7b12b652979e1c0f5d8afa4200
Thibault Carpentier <thibault.carpen@gmail.com>
2018-01-31 11:58:00 -0800

    added cache policy to AFHTTPSessionManager

--- Source/AFHTTPSessionManager+AFUniqueGET.h
+++ Source/AFHTTPSessionManager+AFUniqueGET.h
@@ -4,6 +4,7 @@
 
 - (void)uniqueGET:(NSString *)URLString
        parameters:(id)parameters
+      cachePolicy:(NSURLRequestCachePolicy)cachePolicy
              task:(void (^)(NSURLSessionDataTask *task, BOOL existing))task
           success:(void (^)(NSURLSessionDataTask *task, id responseObject))success
           failure:(void (^)(NSURLSessionDataTask *task, NSError *error))failure;
